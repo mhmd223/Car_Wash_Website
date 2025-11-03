e738475b1daf5cad4d48a8db6e2eec695452a8d2
Yousef Ahmed <92268955+YousefAhmed24@users.noreply.github.com>
2025-11-03 23:55:37 +0800

    Added empty div for testing

---
+++ CarWashApp/src/TestComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TestComponent() {
+  return (
+    <div className="test-container">
+      {/* Empty div for backend testing */}
+    </div>
+  );
+}
+
+export default TestComponent;
