fa6ab040f23ec66bc04ebf43ba5bed66dd884e93
mhmd223 <kaadanman16@gmail.com>
2026-09-25 20:27:38 +0300

    debugging

--- CarWashApp/FE/src/services/account_services.js
+++ CarWashApp/FE/src/services/account_services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API_URL = `${import.meta.env.VITE_API_URL || "http://localhost:5173"}/account/`;
-
+console.log("API_URL:", API_URL);
 export const register = async (
   username,
   email,
